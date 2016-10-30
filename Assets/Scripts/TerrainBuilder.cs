81bfae6e7f9e77fa60ff66f0b91adc75acd86f5d
anon <vazgen1488@mail.ru>
2016-10-30 19:30:22 +0500

    initial

---
+++ Assets/Scripts/TerrainBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainBuilder : MonoBehaviour {
+
+	public GameObject prefab;
+
+	public void Update() 
+	{
+		if(Input.GetMouseButtonUp(0))
+		{
+			Ray ray  = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+
+				if(hit.collider.tag == "Terrain")
+				{
+					Vector3 placePos = hit.point;
+					placePos.y += 0.5f;
+					placePos.x = Mathf.Round(placePos.x);
+					placePos.z = Mathf.Round(placePos.z);
+					Instantiate (prefab, placePos, Quaternion.identity);
+				}
+			}
+		}
+	}
+
+}
